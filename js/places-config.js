94845d36ddb4d76d181fd031faa99cf871a81636
sundayharmony <sundayharmony@users.noreply.github.com>
2026-02-06 14:01:04 -0500

    Configure Algolia Places keys

--- js/places-config.js
+++ js/places-config.js
@@ -1,4 +1,4 @@
 // Algolia Places config (fill in your credentials)
 // Create an Algolia app and copy your Application ID + Search-Only API Key here.
-window.ALGOLIA_APP_ID = 'YOUR_ALGOLIA_APP_ID';
-window.ALGOLIA_API_KEY = 'YOUR_ALGOLIA_SEARCH_ONLY_API_KEY';
+window.ALGOLIA_APP_ID = 'U4IBWP8ZJX';
+window.ALGOLIA_API_KEY = '927ca934a54aed680e1a16d5d9180b0f';
